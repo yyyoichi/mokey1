49ba5c3bb4a4d238b348704353d7ae7052a8ed2a
yyyoichi <origamiichiro@gmail.com>
2022-09-21 08:57:19 +0000

    解析器

---
+++ src/monkey/lexer/lexer_test.go
@@ -0,0 +1,37 @@
+package lexer
+
+import (
+	"testing"
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLinteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LBRACE, ")"},
+		{token.RPAREN, "("},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+	for i, tt:= range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatlaf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.exxpectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
