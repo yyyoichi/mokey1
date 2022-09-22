674977a54c82b08e500243e3b6d8f442facea575
yyyoichi <origamiichiro@gmail.com>
2022-09-22 03:38:19 +0000

    add new keywords

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -38,10 +38,20 @@ const (
 	//キーワード
 	FUNCTION = "FUNCTION"
 	LET  = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 var keywords = map[string]TokenType {
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else" : ELSE,
+	"return": RETURN,
 }
 
 // 識別子かキーワードを返す
