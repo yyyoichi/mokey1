229f774645dc5335db3273afe912d9574f163070
yyyoichi <origamiichiro@gmail.com>
2022-09-22 03:28:28 +0000

    add switch

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -31,12 +31,25 @@ func(l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
+	
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
