d1b566c38c35a6d91647b5cb970006d2c9b031e2
yyyoichi <origamiichiro@gmail.com>
2022-09-22 03:34:18 +0000

    add 一字記号 test

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -13,7 +13,9 @@ func TestNextToken(t *testing.T) {
 	let add = fn(x, y) {
 		x + y;
 	}
-	let result = add(five, ten);`
+	let result = add(five, ten);
+	!-/*5;
+	5 < 10 > 5;`
 
 	tests := []struct {
 		expectedType token.TokenType
@@ -57,8 +59,21 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
-		// {token.INT, "5"},
+
 		// {token.SEMICOLON, ";"},
 		// {token.PLUS, "+"},
 		// {token.LPAREN, "("},
