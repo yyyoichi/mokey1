c7e577ee2486336428f5cab4d30b88ad593c7362
yyyoichi <origamiichiro@gmail.com>
2022-09-22 03:08:56 +0000

    ホワイトスペーススキップ

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -25,6 +25,9 @@ func(l *Lexer) readChar() {
 }
 func(l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -69,6 +72,13 @@ func (l *Lexer) readIdentifier() string {
 		l.readChar()
 	}
 	return l.input[position: l.position]
+}
+
+// ホワイトスペーススキップ
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func isLetter(ch byte) bool {
