93a0f64e33770a3d1893cf5eef363994f95abcd4
yyyoichi <origamiichiro@gmail.com>
2022-09-24 01:16:09 +0000

    add parser test

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -11,12 +11,13 @@ func TestLetStatements(t *testing.T) {
 	input := `
 	let x = 5;
 	let y = 10;
-	let foobar = 838383;`
+	let 838383;`
 
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -63,4 +64,16 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
 	fmt.Printf("'%s'. got=%s", name, s)
 	return true
 
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return 
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
