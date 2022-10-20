9ae38bb71c8d90536446200288c83eb48b30e51f
yyyoichi <origamiichiro@gmail.com>
2022-10-21 07:54:35 +0900

    add callExpression ast

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -214,6 +214,30 @@ func (fl *FunctionLiteral) String() string {
 	return out.String()
 }
 
+type CallExpresstion struct {
+	Token token.Token //(
+	Function Expression // Identier Functionliteral
+	Arguments []Expression
+}
+
+func (ce *CallExpresstion) expressionNode() {}
+func (ce *CallExpresstion) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpresstion) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
+
 
 type BlockStatement struct {
 	Token token.Token // {
