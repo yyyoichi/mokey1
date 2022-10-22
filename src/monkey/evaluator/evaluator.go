9d8f45dcc7fd1ed04b7fc1d5d89b7e8fdbf5a8c8
yyyoichi <origamiichiro@gmail.com>
2022-10-22 16:24:13 +0900

    use pointer

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -15,7 +20,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBoolean(node.Value)
 	}
 	return nil
 }
@@ -28,4 +33,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBoolean(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
